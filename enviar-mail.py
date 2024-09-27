8a2de191b4bd0d7f17a6d419cdc46425fe35d265
João Victor Santana <113112134+JoaoVVSantana@users.noreply.github.com>
2024-09-27 10:23:25 -0300

    Update

--- enviar-mail.py
+++ enviar-mail.py
@@ -1,6 +1,7 @@
 import smtplib
 import os
 import pandas as pd
+import re
 
 # Multipurpose Internet Mail Extensions #
 from email.mime.multipart import MIMEMultipart
@@ -13,7 +14,7 @@ from email import encoders
 #                                                                                                        #
 #                                   ** É necessário atualizar: **                                        #
 #                                                                                                        #         
-#  - O host SMTP (atualmente usando office 365)                                                          #
+#  - O host SMTP (atualmente usando o gmail)                                                     #
 #  - O diretório dos arquivos                                                                            #
 #  - O arquivo de texto, que contém o corpo do Email                                                     #
 #                                                                                                        #
@@ -25,12 +26,18 @@ from email import encoders
 
 def main():
     conectar_smtp()
+    print(emails_invalidos)
 
+#Inserir a assinatura pessoal ou institucional aqui
+assinatura_html = """
+
+"""
 #Digitar o e-mail e senha, pensando em qual servidor SMTP vai ser usado#
-meu_email=input('Digite seu Email: ')
-minha_senha=input('Digite a Senha: ')
+meu_email=''
+email_login=''
+minha_senha=''
 
-
+emails_invalidos = []
 
 
 
@@ -38,63 +45,72 @@ minha_senha=input('Digite a Senha: ')
 def conectar_smtp(): #Cria uma sessão SMTP#
      
     try:
-        with smtplib.SMTP('smtp.office365.com',587) as server:
+        with smtplib.SMTP('smtp.gmail.com',587) as server:
             
             server.starttls() #Criptografia#
             
             print('Realizando login... ')
-            server.login(meu_email,minha_senha) #Realiza login com o e-mail e senha de quem vai enviar#
+            server.login(email_login,minha_senha) #Realiza login com o e-mail e senha de quem vai enviar#
             print('Login Concluido')
 
-            ler_planilha(server)  #Começa o processo de leitura dos arquivos#      
+            ler_planilha(server)  #Começa o processo de leitura da planilha#      
     except Exception as e:
-        print(f'Falha ao conectar no servidor SMTP: {str(e)}') #Verificar qual o erro, servidor gmail apresenta problemas de autenticação#
+        print(f'Falha no servidor SMTP: {str(e)}') #Verificar qual o erro#
 
 
-#--------------------------------------------------------------------------------------------------------#
-#                                                                                                        #
-#                                   ** Padrões da planilha **                                           #
-#                                                                                                        #
-#  - [1,A] = Nome                                                                                        #
-#  - [1,B] = Unidade                                                                                     #
-#  - [1,C] = Setor                                                                                       #
-#  - [1,D] = Email                                                                                       #
-#  - [1,E] = Arquivo                                                                                     #
-#  - [1,F] = Setor                                                                                       #
-#                                                                                                        #
-#--------------------------------------------------------------------------------------------------------#
+#------------------------------------------------------------------------------------------------------#
+#                                                                                                      #
+#                                   ** Padrões da planilha **                                          #
+#                                                                                                      #
+#  - [1,A] = Data/Hora                                                                                 #
+#  - [1,B] = Nome Completo                                                                             #
+#  - [1,C] = Unidade                                                                                   #
+#  - [1,D] = Cargo                                                                                     #
+#  - [1,E] = E-mail                                                                                     #
+#                                                                                                      #
+#------------------------------------------------------------------------------------------------------#
 
 def ler_planilha(server):
 
     
-    lista=pd.read_excel(r'C:\Users\jvict\OneDrive\Desktop\Python\Script_Emails\teste Lista .xlsx')  #Leitura da planilha do excel#
+    lista=pd.read_excel(r'Lista_de_presenca_respostas.xlsx')  #Leitura da planilha do excel#
     
-    lista_filtrada=lista.dropna(subset=['Nome', 'Email', 'Arquivo']) #Filtra a planilha lida pra exibir só as linhas totalmente preenchidas#
+    lista_filtrada=lista.dropna(subset=['Nome Completo', 'E-mail']) #Filtra a planilha lida pra exibir só as linhas totalmente preenchidas#
 
-    
-    for index, row in lista_filtrada.iterrows(): #Itera a planilha filtrada, salvando nome, email e nome do certificado#
+    email_regex = r'^[\w\.-]+@[\w\.-]+\.\w+$' #verificar se o email está no padrão correto
+
+    for i, row in lista_filtrada.iterrows(): #Itera a planilha filtrada, salvando nome, email e nome do certificado#
+        nome_destinatario=row['Nome Completo']
+        email_destinatario=row['E-mail']
+        #Tratando os inválidos
+        if re.match(email_regex, email_destinatario):
+            print(f'Escrevendo para {nome_destinatario}')        
+            escrever_email(nome_destinatario,email_destinatario,server)
+        else:
+            emails_invalidos.append((nome_destinatario, email_destinatario))
+            print(f"Email inválido encontrado: {email_destinatario}. Pulando este destinatário.")
 
         
-        nome_destinatario=row['Nome']
-        
-        email_destinatario=row['Email']
-
-        nome_arquivo=row['Arquivo']
-
-        string_Arq=f'{nome_arquivo}.pdf' #Transforma o nome do arquivo com .pdf pra colocar no caminho relativo
-        print(f'Escrevendo para {nome_destinatario}')        
-        escrever_email(string_Arq,nome_destinatario,email_destinatario,server)
 
 
 
 
-def escrever_email(nome_arq,nome_destinatario,email_destinatario,server):
+def escrever_email(nome_destinatario,email_destinatario,server):
 
     
     conteudo=escrever_mensagem(nome_destinatario) #Salva o nome do destinatário e o conteúdo do arquivo txt#
 
+    corpo_html = f"""
+    <html>
+    <body>
+    <p>{conteudo.replace('\n', '<br>')}</p>
+    {assinatura_html}
+    </body>
+    </html>
+    """
+
     
-    arq_pdf=f'C:/Users/jvict/OneDrive/Desktop/Python/Script_Emails/{nome_arq}' #Gambiarra que muda o nome do relative path pro certificado da pessoa da iteração#
+    arq_excel=r"Relatorios.xlsx"
     
     
 
@@ -102,16 +118,17 @@ def escrever_email(nome_arq,nome_destinatario,email_destinatario,server):
     mensagem=MIMEMultipart()
     mensagem['From']=meu_email
     mensagem['To']=email_destinatario
-    mensagem['Subject']=conteudo
-
-
-    mensagem.attach(MIMEText(conteudo,"plain"))
+    mensagem['Subject']='- Reunião dia XX/XX'
 
     
-    with open(arq_pdf,"rb") as anexo: # Abrindo o arquivo em modo de leitura binária (rb) - "as anexo" é o objeto de arquivo usado para ler os dados#
+
+    mensagem.attach(MIMEText(corpo_html,"html"))
+    
+    
+    with open(arq_excel,"rb") as anexo: # Abrindo o arquivo em modo de leitura binária (rb) - "as anexo" é o objeto de arquivo usado para ler os dados#
 
         
-        base=MIMEBase('application','octet-stream') #Instanciando o MIMEBase, com application/octet-stream para anexar arquivos binários#
+        base = MIMEBase('application', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet')#Instanciando o MIMEBase, com application/octet-stream para anexar arquivos binários#
 
         
         base.set_payload(anexo.read()) #Lê todo conteúdo do arquivo binário e define como o payload (corpo de dados) da parte MIME#
@@ -120,10 +137,10 @@ def escrever_email(nome_arq,nome_destinatario,email_destinatario,server):
     encoders.encode_base64(base) #Convertendo os dados binários em caractéres ASCII#
 
     
-    base.add_header('Content-Disposition ',f"attachment; filename={os.path.basename(arq_pdf)}" ) #Adiciona cabeçalho, especifica como o conteúdo deve ser exibido, indica que o conteúdo deve ser tratado como anexo#
+    base.add_header('Content-Disposition ',f"attachment; filename={os.path.basename(arq_excel)}") #Adiciona cabeçalho, especifica como o conteúdo deve ser exibido, indica que o conteúdo deve ser tratado como anexo#
 
     
-    mensagem.attach(base) #Anexa o PDF ao e-mail#
+    mensagem.attach(base) #Anexa o Excel ao e-mail#
 
     enviar_email(mensagem, email_destinatario,server) #Pega a mensagem MIME, quem vai receber o email e de quem tá mandando#
 
@@ -132,10 +149,10 @@ def escrever_email(nome_arq,nome_destinatario,email_destinatario,server):
 def escrever_mensagem(nome):
 
     
-    caminho_texto=(r'C:\Users\jvict\OneDrive\Desktop\Python\Script_Emails\mensagem.txt') #Pega o arquivo txt, lê e salva o conteúdo numa string#
+    caminho_texto=(r'mensagem.txt') #Pega o arquivo txt, lê e salva o conteúdo numa string#
 
     
-    conteudo=f'Prezado(a) {nome},\n' #A string recebe o nome da pessoa que vai receber o Email#
+    conteudo=f'Prezado(a) {nome}, bom dia!\n' #A string recebe o nome da pessoa que vai receber o Email#
 
     with open(caminho_texto, 'r', encoding='utf-8') as arquivo:
         conteudo_arquivo=arquivo.read()
